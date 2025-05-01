c5d27c555948973a01cc3a0ca703cf403ef8bfbb
Lamia <lamiaaktar1216@gmail.com>
2025-05-02 00:25:07 +0600

    sticky scroll behaviour added

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -19,10 +19,10 @@ const HomeLayout = () => {
         </nav>
       </header>
       <main className="w-11/12 mx-auto my-3  grid grid-cols-12 gap-5">
-        <aside className="col-span-3">
+        <aside className="col-span-3 sticky top-0 h-fit">
           <LeftAside></LeftAside>
         </aside>
-        <section className="main col-span-6">
+        <section className="main col-span-6 sticky top-0 h-fit">
           <Outlet></Outlet>
         </section>
         <aside className="col-span-3">
